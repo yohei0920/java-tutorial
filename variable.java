0b2bf24d607e94fb1d6711f6120db78032e13e8c
yohei.hokari <yohei.hokari@gmail.com>
2024-11-23 09:38:28 +0900

    add: VariableClass

---
+++ variable.java
@@ -0,0 +1,16 @@
+class variable {
+  public static void main(String[] args) {
+    int var = 1;
+    int var01 = 2;
+    int var_01 = 3;
+    int var$01 = 4;
+    // int var-01 = 5;
+    int VAR = 6;
+
+    System.out.println(var);
+    System.out.println(var01);
+    System.out.println(var_01);
+    System.out.println(var$01);
+    System.out.println(VAR);
+  }
+}
